44cac3f4001386a7456bd3b0b4caacd7991218b9
Fred Ngo <fredngo@gmail.com>
2020-06-19 10:29:01 -0400

    Add destroy method to MonstersAPI

--- src/services/MonstersAPI.js
+++ src/services/MonstersAPI.js
@@ -1,22 +1,36 @@
-
-const baseUrl = 'http://react-monsters-api.herokuapp.com/api/monsters';
 const apiKey = 'awesomesauce';
+const baseUrl = 'http://react-monsters-api.herokuapp.com/api/monsters';
+const collectionUrl = `${baseUrl}?api_key=${apiKey}`;
+const memberUrl = (id) => `${baseUrl}/${id}?api_key=${apiKey}`;
 
 const index = () => {
-  const url = `${baseUrl}?api_key=${apiKey}`;
-  return fetch(url)
-    .then( response => response.json() );
+  return fetch(collectionUrl)
+    .then(response => response.json());
 }
 
 const show = (id) => {
-  const url = `${baseUrl}/${id}?api_key=${apiKey}`;
-  return fetch(url)
-    .then( response => response.json() );
+  return fetch(memberUrl(id))
+    .then(response => response.json());
+}
+
+const create = (monster) => {
+  console.log('Create Monster')
+}
+
+const update = (monster) => {
+  console.log('Update Monster')
+}
+
+const destroy = (id) => {
+  return fetch(memberUrl(id), {method: 'DELETE'});
 }
 
 const MonstersAPI = {
   index,
-  show
+  show,
+  create,
+  update,
+  destroy
 }
 
 export default MonstersAPI;
