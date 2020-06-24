3cb8ab26313673836a2c035e22e082f2f195bed0
Fred Ngo <fredngo@gmail.com>
2020-06-24 17:52:29 -0400

    Change from PATCH to PUT

--- src/services/MonstersAPI.js
+++ src/services/MonstersAPI.js
@@ -35,7 +35,7 @@ const create = (monster) => {
 
 const update = (monster) => {
   return fetch(memberUrl(monster.id), {
-      method: 'PATCH',
+      method: 'PUT',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(monster),
     })
