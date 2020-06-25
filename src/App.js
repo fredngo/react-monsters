f51411ed05756f27ac89414c94f98cc78489567e
Fred Ngo <fredngo@gmail.com>
2020-06-25 17:15:00 -0400

    Working with components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
 
 import Monsters from './pages/Monsters/Monsters';
@@ -8,43 +8,61 @@ import MonsterNew from './pages/MonsterNew/MonsterNew';
 import NotFound from './pages/NotFound/NotFound';
 import InternalServerError from './pages/InternalServerError/InternalServerError';
 import Offline from './pages/Offline/Offline';
+import Redirector from './components/Redirector/Redirector';
 
-const App = () => {
-  return (
-    <>
-      <header>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-          <Link className="navbar-brand" to='/'>MfH, Inc.</Link>
-        </nav>
-      </header>
-      
-      <main>
-        <Route path="/" render={ routeProps =>
-          !routeProps.location.state?.notice ? '' :
-          <div className="m-3 alert alert-primary">
-            {routeProps.location.state.notice}
-          </div>}
+class App extends Component {
+
+  state = {
+    redirect: {},
+  }
+
+  setRedirect = redirect => {
+    this.setState({redirect});
+  }
+
+  render() {
+    return (
+      <>
+        <Route 
+          render={ routeProps => <Redirector redirect={this.state.redirect} setRedirect={this.setRedirect} {...routeProps} />}
         />
 
-        <Switch>
-          <Route path="/monsters/new" component={MonsterNew} />
-          <Route path="/monsters/:id/edit"
-            render={ routeProps => <MonsterEdit {...routeProps} />}
+        <header>
+          <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <Link className="navbar-brand" to='/'>MfH, Inc.</Link>
+          </nav>
+        </header>
+        
+        <main>
+          <Route path="/" render={ routeProps =>
+            !routeProps.location.state?.alert ? '' :
+            <div className="m-3 alert alert-primary">
+              {routeProps.location.state.alert}
+            </div>}
           />
-          <Route path="/monsters/:id"
-            render={ routeProps => <Monster {...routeProps} />}
-          />
-          <Route exact path="/"
-            render={ routeProps => <Monsters {...routeProps} />}
-          />
-          <Route path="/404" component={NotFound} />
-          <Route path="/500" component={InternalServerError} />
-          <Route path="/offline" component={Offline} />
-          <Route component={NotFound} />
-        </Switch>
-      </main>
-    </>
-  );
+  
+          <Switch>
+            <Route path="/monsters/new"
+              render={ () => <MonsterNew setRedirect={this.setRedirect} />}
+            />
+            <Route path="/monsters/:id/edit"
+              render={ routeProps => <MonsterEdit setRedirect={this.setRedirect} {...routeProps} />}
+            />
+            <Route path="/monsters/:id"
+              render={ routeProps => <Monster setRedirect={this.setRedirect} {...routeProps} />}
+            />
+            <Route exact path="/"
+              render={ routeProps => <Monsters setRedirect={this.setRedirect} {...routeProps} />}
+            />
+            <Route path="/404" component={NotFound} />
+            <Route path="/500" component={InternalServerError} />
+            <Route path="/offline" component={Offline} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Redirector/Redirector.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Redirector extends Component {
+  
+  componentDidUpdate() {
+    const {redirect, location, history, setRedirect} = this.props;
+    const {path, alert} = redirect;
+
+    if (path && path !== location.pathname) {
+      history.push({
+        pathname: path,
+        state: {alert}
+      });
+    }
+    else if (path === location.pathname) {
+      setRedirect({});
+    }
+  }
+
+  render() {
+    return <></>;
+  }
+}
+
+export default Redirector;
