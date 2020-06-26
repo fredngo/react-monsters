2756839169de0702a3ab68fbe844b0845c467423
Fred Ngo <fredngo@gmail.com>
2020-06-26 16:59:48 -0400

    Install Parse library

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Parse from 'parse';
 import dotenv from 'dotenv';
+
 dotenv.config();
+
+Parse.initialize(process.env.REACT_APP_PARSE_APPLICATION_ID, process.env.REACT_APP_PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL;
 
 ReactDOM.render(
   <React.StrictMode>
