1d4bc25190c1631f84e62f32f304d97425d9e0e0
Fred Ngo <fredngo@gmail.com>
2020-07-06 16:55:46 -0400

    Uninstall Parse SDK, will do manually

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Parse from 'parse';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-Parse.initialize(process.env.REACT_APP_PARSE_APPLICATION_ID, process.env.REACT_APP_PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL;
 
 ReactDOM.render(
   <React.StrictMode>
