e31ec8c7fd92113bf2e670f3ee2f6bf6d99664cb
Fred Ngo <fredngo@gmail.com>
2021-03-29 22:51:59 -0400

    Cleanup

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,11 +1,9 @@
 const NotFound = () => (
-  <>
-    <section className="jumbotron">
-      <div className="container">
-        <h1 className="display-4">404 - Not Found</h1>
-      </div>
-    </section>
-  </>
+  <section className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">404 - Not Found</h1>
+    </div>
+  </section>
 );
 
 export default NotFound;
