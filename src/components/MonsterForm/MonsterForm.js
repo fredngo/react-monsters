e41c04151a411dd60a5346c4c72d6edf4bba2cdb
Fred Ngo <fredngo@gmail.com>
2021-03-29 22:33:51 -0400

    Convert MonsterForm to use Hooks

--- src/components/MonsterForm/MonsterForm.js
+++ src/components/MonsterForm/MonsterForm.js
@@ -1,108 +1,98 @@
-import {Component} from 'react';
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
 
-class MonsterForm extends Component {
+const MonsterForm = ({monster, setMonster, callApi, buttonText, redirectNotice, redirectTo, setRedirect, setModal, cancelPath}) => {
 
-  state = {
-    errors: [],
-  }
+  const [errors, setErrors] = useState([]);
 
-  handleChange = e => 
-    this.props.setMonster({
-      ...this.props.monster, 
+  const handleChange = e => 
+    setMonster({
+      ...monster, 
       [e.target.name]: e.target.value
     });
 
-  handleSubmit = async e => {
+  const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const {data, errors} = await this.props.callApi();
+      const {data, errors} = await callApi();
 
-      if (data) {
-        this.props.setRedirect({
-          path: this.props.redirectTo(data),
-          alert: this.props.redirectNotice
+      if (data)
+        setRedirect({
+          path: redirectTo(data),
+          alert: redirectNotice
         });
-      }
-      else {
-        this.setState({errors});
-      }
+      else
+        setErrors(errors);
     }
     catch {
-      this.props.setModal('offline');
+      setModal('offline');
     }
   }
 
-  render() {
-    const {monster, buttonText, cancelPath} = this.props;
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          {
-            !this.state.errors.length ? null :
-            <div className="alert alert-warning">
-              <h2>{this.state.errors.length} error(s) prohibited this monster from being saved:</h2>
+  return (
+    <form onSubmit={handleSubmit}>
+      {
+        !errors.length ? null :
+        <div className="alert alert-warning">
+          <h2>{errors.length} error(s) prohibited this monster from being saved:</h2>
+          <ul>
+            { errors.map( (m, i) => <li key={i}>{m}</li> ) }
+          </ul>
+        </div>
+      }
 
-              <ul>
-                { this.state.errors.map( (m, i) => <li key={i}>{m}</li> ) }
-              </ul>
-            </div>
-          }
-
-          <div className="form-group">
-            <label>
-              <span>Name</span>
-              <input
-                className="form-control"
-                name="name"
-                defaultValue={monster.name}
-                onChange={this.handleChange}
-                // required
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              <span>Home</span>
-              <input
-                className="form-control"
-                name="home"
-                defaultValue={monster.home}
-                onChange={this.handleChange}
-                // required
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              <span>Creepiness</span>
-              <input
-                className="form-control"
-                type="number"
-                name="creepiness"
-                defaultValue={monster.creepiness}
-                onChange={this.handleChange}
-                // required
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              <span>Bio</span>
-              <textarea
-                className="form-control"
-                name="bio"
-                defaultValue={monster.bio}
-                onChange={this.handleChange}
-              />
-            </label>
-          </div>
-          <button className="btn btn-primary">{buttonText}</button>
-          <Link className="btn btn-link" to={cancelPath}>Cancel</Link>
-        </form>
-      </>
-    );
-  }
+      <div className="form-group">
+        <label>
+          <span>Name</span>
+          <input
+            className="form-control"
+            name="name"
+            defaultValue={monster.name}
+            onChange={handleChange}
+            // required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          <span>Home</span>
+          <input
+            className="form-control"
+            name="home"
+            defaultValue={monster.home}
+            onChange={handleChange}
+            // required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          <span>Creepiness</span>
+          <input
+            className="form-control"
+            type="number"
+            name="creepiness"
+            defaultValue={monster.creepiness}
+            onChange={handleChange}
+            // required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          <span>Bio</span>
+          <textarea
+            className="form-control"
+            name="bio"
+            defaultValue={monster.bio}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <button className="btn btn-primary">{buttonText}</button>
+      <Link className="btn btn-link" to={cancelPath}>Cancel</Link>
+    </form>
+  );
 }
 
 export default MonsterForm;
